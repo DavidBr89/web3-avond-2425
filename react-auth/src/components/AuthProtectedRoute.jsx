de7f86c06420798a7f80ead1bb6714f6279a74a1
David Breckx <DavidBr89@users.noreply.github.com>
2024-12-10 18:17:09 +0100

    Added react-auth project

---
+++ react-auth/src/components/AuthProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const AuthProtectedRoute = (props) => {};
+
+export default AuthProtectedRoute;
---
+++ react-auth/src/pages/RegisterPage.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import TextInput from "../components/TextInput";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { useAuth } from "../contexts/AuthContext";
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("Voornaam is verplicht"),
+  lastName: Yup.string().required("Achternaam is verplicht"),
+  email: Yup.string()
+    .required("Email is verplicht")
+    .email("Geen geldig email adres"),
+  password: Yup.string()
+    .required("Wachtwoord is verplicht")
+    .min(8, "Wachtwoord moet uit minstens 8 karakters bestaan"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Wachtwoorden komen niet overeen")
+    .required("Wachtwoord bevestiging verplicht"),
+});
+
+const RegisterPage = () => {
+  const { register } = useAuth();
+  const { touched, errors, values, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+      onSubmit: ({ email, firstName, lastName, password }) => {
+        register(firstName, lastName, email, password);
+      },
+      validationSchema: validationSchema,
+    });
+
+  return (
+    <div className="mt-4 flex flex-col gap-8">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <TextInput
+          name="firstName"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.firstName}
+          error={touched.firstName && errors.firstName}
+          errorlabel={touched.firstName && errors.firstName}
+          placeholder="Voornaam"
+        />
+        <TextInput
+          name="lastName"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.lastName}
+          error={touched.lastName && errors.lastName}
+          errorlabel={touched.lastName && errors.lastName}
+          placeholder="Achternaam"
+        />
+        <TextInput
+          name="email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          error={touched.email && errors.email}
+          errorlabel={touched.email && errors.email}
+          placeholder="Email"
+        />
+        <TextInput
+          name="password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          error={touched.password && errors.password}
+          errorlabel={touched.password && errors.password}
+          placeholder="Wachtwoord"
+          type="password"
+        />
+        <TextInput
+          name="confirmPassword"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.confirmPassword}
+          error={touched.confirmPassword && errors.confirmPassword}
+          errorlabel={touched.confirmPassword && errors.confirmPassword}
+          placeholder="Bevestiging wachtwoord"
+          type="password"
+        />
+        <button className="bg-black hover:bg-neutral-800 px-4 py-2 text-white rounded-lg">
+          Registreren
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
